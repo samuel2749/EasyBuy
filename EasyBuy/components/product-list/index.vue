ca45118a1e3f894a0180d1aa4e13a2ee03f898c6
Samuel <samuel@obdesign.com.tw>
2018-12-27 18:15:23 +0800

    建立產品清單的vue

---
+++ EasyBuy/components/product-list/index.vue
@@ -0,0 +1,20 @@
+﻿<template>
+    <div class="hello">Hello {{who}}</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    },
+    mounted() {
+        console.log("vue is ready");
+    },
+}
+</script>
+
+<style>
+@import "product-list.css";
+</style>
